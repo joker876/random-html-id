376463ec2bfc81a5bb859154062c05d170d10eee
joker876 <42680429+joker876@users.noreply.github.com>
2022-09-20 14:03:04 +0200

    greatly improved both functions

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
-import { v4 } from 'uuid';
+import shortUuid from 'short-uuid';
 import { TakeChance } from 'take-chance';
 
-const ALL_LETTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const ALL_NUMBERS = '0123456789'.split('');
-const ID_SPECIAL_CHARS = '_-'.split('');
-
 export function id(length: number = 16): string {
-    return TakeChance.id();
+    return TakeChance.id(length);
 }
 export function fromUuid(): string {
-    let uuidBuffer = new Uint8Array(16);
-    uuidBuffer = v4(undefined, uuidBuffer);
-    console.log(uuidBuffer);
-    
-    let ret = 'a';
-    for (const byte of uuidBuffer) {
-        ret += byte.toString(36);
-    }
-    return ret;
+    return 'a' + shortUuid.generate();
 }
-
-fromUuid();
